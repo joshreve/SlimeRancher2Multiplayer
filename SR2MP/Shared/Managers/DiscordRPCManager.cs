81115b2649bd15afc30127d9d9548d94c006272a
MadMincraftr <thean.mcgarity@gmail.com>
2026-04-26 20:14:38 -0500

    fix?

--- SR2MP/Shared/Managers/DiscordRPCManager.cs
+++ SR2MP/Shared/Managers/DiscordRPCManager.cs
@@ -113,7 +113,7 @@ internal static class DiscordRPCManager
 
     public static ZoneDefinition? currentZone;
 
-    public static bool IsInEndingCutscene => SystemContext.Instance.SceneLoader._currentSceneGroup.name == "OutroSequence";
+    public static bool IsInEndingCutscene => SystemContext.Instance.SceneLoader._currentSceneGroup?.name == "OutroSequence";
     public static bool IsFightingFinalBoss => BossFightController.Instance?._bossFightIsActive == true;
 
     internal static void UpdatePresence()
