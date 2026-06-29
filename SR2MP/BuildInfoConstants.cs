6d13cef96d507232ffa515631a9957c600f9afa4
Joshua Shreve <j.a.shreve@gmail.com>
2026-06-28 20:36:55 -0400

    Bump version to 0.4.0, update ID to ranchingparty, rename mod to Ranching Party

--- SR2MP/BuildInfoConstants.cs
+++ SR2MP/BuildInfoConstants.cs
@@ -3,9 +3,9 @@
 
 internal static class BuildInfo
 {
-    internal const string ID = "joshreve.ranchingtogether";
-    internal const string Name = "Ranching Together";
-    internal const string Description = "Adds Multiplayer to Slime Rancher 2";
+    internal const string ID = "joshreve.ranchingparty";
+    internal const string Name = "Ranching Party";
+    internal const string Description = "Adds Multiplayer to Slime Rancher 2. Major rewrite of Ranching Together!";
     internal const string Author = "joshreve";
     internal static readonly string[] CoAuthors = new[] { "Shark" };
     internal static readonly string[] Contributors = new[] { "AlchlcSystm, PinkTarr" };
@@ -14,9 +14,9 @@ internal static class BuildInfo
     // Version automatically gets a -dev at the end if SR2MP is compiled by GitHub Action
     internal const string MelonVersion = "0.3.3";
     internal const string Version = "0.4.0"; // Auto-Dev_Do_not_remove
-    internal const string Discord = "https://discord.com/invite/a7wfBw5feU";
+    internal const string Discord = "None";
     internal const string SourceCode = "https://github.com/joshreve/SlimeRancher2Multiplayer";
-    internal const string Nexus = "https://www.nexusmods.com/slimerancher2/mods/118";
+    internal const string Nexus = "None";
     internal const bool UsePrism = false;
     internal const string MinimumStarlightVersion = Starlight.BuildInfo.CodeVersion; // e.g "3.6.3", the min required SR2 version. No beta or alpha versions
     internal const string MinimumGameVersion = "1.2.3"; // e.g 1.1.0 or something similar (optional)
